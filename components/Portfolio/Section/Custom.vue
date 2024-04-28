35d9bf66bdc52bd2385de8c1210488ba47da61df
erikleisinger <erikleisinger@gmail.com>
2024-04-28 15:27:51 -0400

    remove lazy load

--- components/Portfolio/Section/Custom.vue
+++ components/Portfolio/Section/Custom.vue
@@ -5,7 +5,7 @@
         <a class="text-h2 uppercase text-gold underline font-bold cursor-pointer" v-if="content.link" @click="navigateTo(content.link.href)">{{ content.link.text }}</a>
         <p v-if="content.description" >{{ content.description }}</p>
     </header>
-        <NuxtImg v-for="img, index in content.imgs" :key="index" :src="img"  fit="outside" class="m-auto [&:not(:first-of-type)]:mt-4" loading="lazy"/>
+        <NuxtImg v-for="img, index in content.imgs" :key="index" :src="img"  fit="outside" class="m-auto [&:not(:first-of-type)]:mt-4" />
       <video-player v-if="content.video" :video="content.video"/>
     </section>
 </template>
