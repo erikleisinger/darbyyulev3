58b57dd5f65f6ddacfaa74688e932bd7bc3d471c
erikleisinger <erikleisinger@gmail.com>
2024-05-23 19:18:23 -0400

    adjust font size of icons

--- components/Portfolio/Section/ProblemOutcome.vue
+++ components/Portfolio/Section/ProblemOutcome.vue
@@ -4,7 +4,7 @@
 
       <h3 class="flex items-center gap-2 font-bold">
         <div>
-        <div class="i-carbon-help-filled   mt-[0.1em]  text-[4.2em] md:text-[1.1em] mr-2 " />
+        <div class="i-carbon-help-filled   mt-[0.1em]  text-[1.5em] md:text-[1.1em] mr-2 " />
       </div>
         <span class="leading-[1.2]">
         <span class=" text-mustard" role="h2">Problem:</span>
@@ -16,7 +16,7 @@
  
       <h3 class="flex items-center gap-2 mt-5 font-bold">
         <div>
-        <div class="i-carbon-radio-button-checked   mt-[0.1em] text-[3em] md:text-[1.1em] mr-2" />
+        <div class="i-carbon-radio-button-checked   mt-[0.1em] text-[1.5em] md:text-[1.1em] mr-2" />
       </div>
         <span class="leading-[1.2]">
         <span class=" text-mustard " role="h2">Outcomes:</span>
