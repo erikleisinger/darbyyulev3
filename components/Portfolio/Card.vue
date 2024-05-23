d4e6f663c7ed831e344978c79a2e87a9ef478a3c
erikleisinger <erikleisinger@gmail.com>
2024-05-23 14:34:06 -0400

    change nuxtimg to img on cards

--- components/Portfolio/Card.vue
+++ components/Portfolio/Card.vue
@@ -1,7 +1,7 @@
 
 <template>
     <article class="grid grid-cols-[auto,1fr] min-h-[100px] md:min-h-[300px] overflow-hidden md:hover:bg-black/5 transition-all cursor-pointer gap-2 md:gap-0">
-        <NuxtImg :src="item.img_main" class="h-full  w-[150px] object-cover" loading="lazy" />
+        <img :src="item.img_main" class="h-full  w-[150px] object-cover"  />
         <div class="md:p-6">
             <h2 class="text-gold md:normal-case font-medium md:font-bold">{{ item.name }}</h2>
             <p class="text-p3">{{ item.description }}</p>
